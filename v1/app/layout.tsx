7576cdb2733a67c20bf380c32690fe8fba8cbb35
Dakotah Godfrey <dakotah.godfrey.dev@gmail.com>
2024-02-26 13:08:59 -0500

    Update dependencies and fix styling issues

--- v1/app/layout.tsx
+++ v1/app/layout.tsx
@@ -1,23 +1,27 @@
-import classNames from 'classnames'
-import './globals.css'
-import "@agility/plenum-ui/lib/tailwind.css"
-import { Mulish } from 'next/font/google'
+import classNames from "classnames";
+import "./globals.css";
+import "@agility/plenum-ui/dist/tailwind.css";
+import { Mulish } from "next/font/google";
 
-const mulish = Mulish({subsets: ["latin"]})
+const mulish = Mulish({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Agility CMS BigCommerce App',
-  description: 'Connect your BigCommerce store to Agility CMS',
-}
+  title: "Agility CMS BigCommerce App",
+  description: "Connect your BigCommerce store to Agility CMS",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-		<html lang="en" className='h-full bg-white'>
-			<body className={classNames(mulish.className, "bg-white h-full text-black")} >{children}</body>
-		</html>
-	)
+    <html lang="en" className="h-full bg-white">
+      <body
+        className={classNames(mulish.className, "bg-white h-full text-black")}
+      >
+        {children}
+      </body>
+    </html>
+  );
 }
